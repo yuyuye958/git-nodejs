e710e20a16f9eef9647dcf0df37b1f59ef76e77d
Allen <yuyuye958@qq.com>
2018-09-11 17:17:48 +0800

    .

--- demo.sh
+++ demo.sh
@@ -9,7 +9,7 @@ else
 
   echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" >> index.html
 
-  echo -e " h1{color: red;}" >> css/style.css
+  echo -e "h1{color: red;}" >> css/style.css
 
   echo -e "var string = "Hello World"\nalert(string)" >> js/main.js
 
